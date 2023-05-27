1d47ca99adb60e6d96537d33321ef3d102fc5c9e
xlepotato <yucia181@gmail.com>
2023-05-27 16:30:53 +0800

    feat(budgetify): overview with ChartJS

--- static/main.js
+++ static/main.js
@@ -1,6 +1,32 @@
+// Navbar
 document.querySelectorAll(".nav-link").forEach((link) => {
     if (link.href === window.location.href) {
         link.classList.add("active");
         link.setAttribute("aria-current", "page");
     }
 });
+// End of Navbar
+
+// Pie Chart
+// Retrieve the value after gaining access to the relevant HTML tag
+let food = document.getElementById('tfood').innerHTML
+let entertainment = document.getElementById('tentertainment').innerHTML
+let business = document.getElementById('tbusiness').innerHTML
+let other = document.getElementById('tother').innerHTML
+let colors = [
+    '#ff80aa', '#99ffcc', '#99ffff', '#ffb3ec'
+];
+const ctx = document.getElementById('myChart');
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Food', 'Entertainment', 'Business', 'Other'],
+        datasets: [{
+            label: 'Amount (in dollars)',
+            data: [food, entertainment, business, other],
+            backgroundColor: colors,
+            borderWidth: 1
+        }]
+    }
+});
+// End of Pie Chart
