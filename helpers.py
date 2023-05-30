8263ed05461925c0d56bbbcb824784f464828aed
xlepotato <yucia181@gmail.com>
2023-05-30 22:37:55 +0800

    feat(budgetify): server-side form fields validation for add and edit

--- helpers.py
+++ helpers.py
@@ -1,3 +1,11 @@
+from flask import render_template
+
+
+def error(message, code=400):
+    """Render message as an error to user."""
+    return render_template("error.html", code=code, message=message), code
+
+
 def usd(value):
     """Format value as USD."""
     return f"${value:,.2f}"
